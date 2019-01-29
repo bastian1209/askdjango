fca9f1e1bdd5ae7f8dc1dc987e44382e6c67f4e4
bastian1209 <bastian95@snu.ac.kr>
2019-01-30 03:40:35 +0900

    template 상속

--- accounts/models.py
+++ accounts/models.py
@@ -5,5 +5,8 @@ from django.conf import settings
 class Profile(models.Model):
     user=models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     # user=models.OneToOneField(User,on_delete=models.CASCADE)
+    # username=models.CharField(max_length=20)
+    # email=models.CharField(max_length=20)
     phone_number=models.CharField(max_length=20)
     address=models.CharField(max_length=50)
+    # date_joined=models.DateTimeField(auto_now_add=True)
