3d86113c8d6b61db3d72165a5ccee5161c9c7988
bastian1209 <bastian95@snu.ac.kr>
2019-01-30 13:51:58 +0900

    post_form views_cbv

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from . import views_cbv
 
 app_name='blog' #include에 namespace인자를 주면 urls.py에 app_name을 명시해줘야함
 
 urlpatterns=[
     #최상위 주소
     path('',views.post_list,name='post_list'),
-    path('<int:pk>/',views.post_detail,name='post_detail')
+    path('<int:pk>/',views.post_detail,name='post_detail'),
+    path('cbv/new/',views_cbv.post_new),
 ]
