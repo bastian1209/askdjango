d1ea185767fad60f9fa3d0f19bb8ca11cb2e0097
bastian1209 <bastian95@snu.ac.kr>
2019-02-01 15:48:31 +0900

    post_edit 적용

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404, redirect
 from .models import Post
 from django.http import Http404
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -18,4 +19,31 @@ def post_detail(request,pk):
     post=get_object_or_404(Post,pk=pk)
     return render(request,'blog/post_detail.html',{
         'post':post,
+    })
+
+def post_new(request):
+    if request.method=='POST':
+        form=PostForm(request.POST,request.FILES)
+        if form.is_valid():
+            post=form.save()
+            return redirect(post) #post.get_absolute_url
+    else:
+        form=PostForm()
+
+    return render(request,'blog/post_form.html',{
+        'form':form,
+    })
+
+def post_edit(request,id):
+    post=get_object_or_404(Post,id=id )
+    if request.method=='POST':
+        form=PostForm(request.POST,request.FILES,instance=post)
+        if form.is_valid():
+            post=form.save()
+            return redirect(post) #post.get_absolute_url
+    else:
+        form=PostForm(instance=post)
+
+    return render(request,'blog/post_form.html',{
+        'form':form,
     })
