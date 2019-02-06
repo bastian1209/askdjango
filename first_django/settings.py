2f39faaa343fe15768d0f3dd9c3118a191a4fa40
bastian1209 <bastian95@snu.ac.kr>
2019-02-06 19:31:50 +0900

    static 추가

--- first_django/settings.py
+++ first_django/settings.py
@@ -133,7 +133,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = ' /static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'first_django','static'),
+]
 
 INTERNAL_IPS=['127.0.0.1']
 
