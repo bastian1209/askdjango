b13f918c23f064feceb38a37e57a2b6e2350c42d
bastian1209 <bastian95@snu.ac.kr>
2019-02-07 00:51:13 +0900

    @login_required

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from django.conf import settings
 # Create your views here.
+@login_required
 def profile(request):
     return render(request,'accounts/profile.html')
 
