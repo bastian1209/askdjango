e011137c8707e19fa4fa3b0d30fef0b52d499a82
bastian1209 <bastian95@snu.ac.kr>
2019-02-06 20:11:18 +0900

    media root 추가

--- first_django/settings.py
+++ first_django/settings.py
@@ -134,11 +134,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = ' /static/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'first_django','static'),
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'first_django', 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-INTERNAL_IPS=['127.0.0.1']
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+INTERNAL_IPS = ['127.0.0.1']
 
 from django.contrib.messages import constants
 
