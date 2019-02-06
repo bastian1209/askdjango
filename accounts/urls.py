b96995f470b551a08c798ab534707870f14aa6a5
bastian1209 <bastian95@snu.ac.kr>
2019-02-07 01:18:28 +0900

    퀴즈 구현 실패

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,13 +2,17 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 from django.conf import settings
+from .forms import LoginForm
 app_name='accounts'
 
 urlpatterns=[
 
     path('signup/',views.signup, name='signup'),
     path('login/',auth_views.LoginView.as_view(),name='login',
-         kwargs={'template_name':'registration/login.html'}),
+         kwargs={
+             'template_name': 'registration/login.html',
+             'authentication_form':LoginForm,
+         }),
     path('logout/',auth_views.LogoutView.as_view(),name='logout',
          kwargs={'next_page':settings.LOGIN_URL}),
     path('profile/',views.profile,name='profile'),
